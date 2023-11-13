aaefbc53dc9b4987850d576d649696a61847c4dc
malakhm <malakhmm22@gmail.com>
2023-11-13 10:47:15 +0200

    login routes done

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -2,32 +2,54 @@ const Admin = require('../Models/adminModel');
 
 const mongoose = require('mongoose');
 
-const authenticate = async(req,res) =>{
+// const authenticate = async(req,res) =>{
     // check if the request has a username and password in the headers
-   const { username, password } = req.body
+//    const { username, password } = req.body
 
-    try{
-        // Query the data base to find an admin with the same username and password
-        const admin = await Admin.findOne({username, password});
+//     try{
+//         // Query the data base to find an admin with the same username and password
+//         const admin = await Admin.findOne({username, password});
 
-        if(admin){
-            // admin with the provided username and password found
-            res.status(200).json({message:'Admin access granted'});
-            return true
-        }else{
-            // No admin with matching credetials found
-            res.status(400).json({error:'access denied'})
-            return false
-        }
+//         if(admin){
+//             // admin with the provided username and password found
+//             res.status(200).json({message:'Admin access granted'});
+//             return true
+//         }else{
+//             // No admin with matching credetials found
+//             res.status(400).json({error:'access denied'})
+//             return false
+//         }
 
 
-    } 
-    catch(error) {
-        // Handle any database query errors here
-        res.status(500).json({error:'Internal server error'});
+//     } 
+//     catch(error) {
+//         // Handle any database query errors here
+//         res.status(500).json({error:'Internal server error'});
+//     }
+
+// };
+
+const loginAdmin = async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ userName });
+
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
     }
 
+    if (password === admin.password) {
+      res.status(200).json({ status: "ok", data: admin });
+    } else {
+      res.status(401).json({ error: "Incorrect password" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve the admin" });
+  }
 };
+
 
 
 
@@ -41,4 +63,4 @@ const addAdmin =  async (req, res) => {
     }
   }
 
-module.exports = {authenticate, addAdmin};
+module.exports = {loginAdmin, addAdmin};
